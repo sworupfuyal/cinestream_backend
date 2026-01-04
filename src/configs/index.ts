6a25aa45049925cd94f78215e38debe5bdb007c4
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-04 23:12:29 +0545

    configs folder

---
+++ src/configs/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PORT: number = 
+    process.env.PORT ? parseInt(process.env.PORT) : 5050;
+export const MONGODB_URI: string = 
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb';
+
+export const JWT_SECRET: string = 
+    process.env.JWT_SECRET || 'your_jwt_secret_key'
