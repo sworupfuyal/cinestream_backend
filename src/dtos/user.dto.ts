4030f0ec6e5fbaa30a9b9d20fa6158401e7a7bdd
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-05 14:15:48 +0545

    new dtos

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+import { UserSchema } from "../types/user.type";
+// re-use UserSchema from types
+export const CreateUserDTO = UserSchema.pick(
+    {
+        email: true,
+        fullname: true,
+        password: true
+    }
+).extend( // add new attribute to zod
+    {
+        confirmPassword: z.string().min(6)
+    }
+).refine( // extra validation for confirmPassword
+    (data) => data.password === data.confirmPassword,
+    {
+        message: "Passwords do not match",
+        path: ["confirmPassword"]
+    }
+)
+export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
+
+export const LoginUserDTO = z.object({
+    email: z.email(),
+    password: z.string().min(6)
+});
+export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
