f71df8f0db4ac3464a6b5cee57e193ac577743e0
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-05 14:17:28 +0545

    new types

---
+++ src/types/user.type.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const UserSchema = z.object({
+    fullname: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(6),
+    role: z.enum(["user", "admin"]).default("user"),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
