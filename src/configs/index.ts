fd190cf695cd8911774c62b875e39501680610ce
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-30 21:28:16 +0545

    index

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
+import cors from 'cors';
 dotenv.config();
 
 export const PORT: number = 
-    process.env.PORT ? parseInt(process.env.PORT) : 5050;
+    process.env.PORT ? parseInt(process.env.PORT) : 6050;
 export const MONGODB_URI: string = 
     process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb';
 
