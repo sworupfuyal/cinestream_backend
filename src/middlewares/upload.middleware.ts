dae340ca8f5ee9a1a20e847e357075e6872bfb80
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-02-14 21:47:45 +0545

    middlewares

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,6 +1,8 @@
 import multer from "multer";
 import path from "path";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4
+  
+ } from "uuid";
 import fs from 'fs';
 import { Request } from "express";
 import { HttpError } from "../errors/http-error";
